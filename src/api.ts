59930ec677599e50f519c89e11556fe78060a62b
adnen rebai <adnen.rebai@gmail.com>
2021-12-11 01:23:58 +0100

    add endpoint api

---
+++ src/api.ts
@@ -0,0 +1,31 @@
+import { AppState } from "./state/appStateReducer";
+
+const MAIN_URL = process.env.REACT_APP_BACKEND_ENDPOINT;
+
+export const save = (payload: AppState) => {
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify(payload),
+  };
+  return fetch(`${MAIN_URL}/save`, options).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Error while saving the state");
+    }
+  });
+};
+
+export const load = () => {
+  return fetch(`${MAIN_URL}/load`).then((response) => {
+    if (response.ok) {
+      return response.json() as Promise<AppState>;
+    } else {
+      throw new Error("Error while loading the state.");
+    }
+  });
+};
