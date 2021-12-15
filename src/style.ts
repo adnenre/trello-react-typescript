f7a8977ecc16eb27053edcd7eb68bd81b3a38714
adnen rebai <adnen.rebai@gmail.com>
2021-12-15 23:33:52 +0100

    fix max with while dragging

--- src/style.ts
+++ src/style.ts
@@ -77,7 +77,7 @@ export const CardContainer = styled(DragPreviewContainer)`
   cursor: pointer;
   margin-bottom: 0.5rem;
   padding: 0.5rem 1rem;
-
+  max-width: 280px;
   height: 100px;
 
   box-shadow: #06060617 2px 2px 8px 0px;
