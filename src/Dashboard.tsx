c6f9da27f74c042907bc6067a43b5aebe3e514ce
adnen rebai <adnen.rebai@gmail.com>
2021-12-21 05:00:34 +0100

    create a new component for trello Dashboard

---
+++ src/Dashboard.tsx
@@ -0,0 +1,41 @@
+import { AppContainer, BodyContainer, AppHeader } from "./style";
+import AddNewItem from "./AddNewItem";
+import { Column } from "./Column";
+import { addList } from "./state/actions";
+import { useAppState } from "./state/AppStateContext";
+import { CustomDragLayer } from "./CustomDragLayer";
+import TrelloLogo from "./Components/Logo";
+const Dashboard = () => {
+  const { lists, dispatch } = useAppState();
+
+  /**
+   * HANDLE ADD COLUMN
+   * @param text column Name
+   */
+  const handleAddColumn = (text: string) => {
+    dispatch(addList(text));
+  };
+  return (
+    <>
+      <AppContainer>
+        <AppHeader>
+          <TrelloLogo />
+        </AppHeader>
+
+        <BodyContainer>
+          <CustomDragLayer />
+          {lists.map(({ text, id }) => (
+            <Column key={id} text={text} id={id} />
+          ))}
+          <AddNewItem
+            inputPlaceHolder="Enter a title for your column..."
+            toggleButtonText="+ Add new Column "
+            onAdd={handleAddColumn}
+          />
+        </BodyContainer>
+      </AppContainer>
+    </>
+  );
+};
+
+export default Dashboard;
