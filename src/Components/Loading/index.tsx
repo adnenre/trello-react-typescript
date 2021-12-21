c3395b8661e7d309e3fc8327d28830440de9807d
adnen rebai <adnen.rebai@gmail.com>
2021-12-21 05:04:10 +0100

    refactor Loading component

--- src/Components/Loading/index.tsx
+++ src/Components/Loading/index.tsx
@@ -1,12 +1,15 @@
 import { LoadingContainer, LoadingText, Spinner } from "./Loading.styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faNetworkWired } from "@fortawesome/free-solid-svg-icons";
 
 type LoadingProps = {
   message?: string;
+  error?: boolean;
 };
-const Loading = ({ message }: LoadingProps) => {
+const Loading = ({ message, error }: LoadingProps) => {
   return (
     <LoadingContainer>
-      <Spinner />
+      {error ? <FontAwesomeIcon icon={faNetworkWired} /> : <Spinner />}
       <LoadingText>{message}</LoadingText>
     </LoadingContainer>
   );
