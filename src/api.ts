62d5041516f74f5e70b486b837feb247ed81ea7e
adnen rebai <adnen.rebai@gmail.com>
2021-12-21 04:59:51 +0100

    usage of axios insted of fetch

--- src/api.ts
+++ src/api.ts
@@ -1,31 +1,16 @@
+import axios from "axios";
 import { AppState } from "./state/appStateReducer";
 
 const MAIN_URL = process.env.REACT_APP_BACKEND_ENDPOINT;
 
 export const save = (payload: AppState) => {
-  const options = {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-type": "application/json",
-    },
-    body: JSON.stringify(payload),
-  };
-  return fetch(`${MAIN_URL}/save`, options).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Error while saving the state");
-    }
-  });
+  return axios
+    .post<AppState>(`${MAIN_URL}/save`, payload)
+    .then((response) => response);
 };
 
 export const load = () => {
-  return fetch(`${MAIN_URL}/load`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Error while saving the state");
-    }
-  });
+  return axios
+    .get<AppState>(`${MAIN_URL}/load`)
+    .then((response) => response.data);
 };
