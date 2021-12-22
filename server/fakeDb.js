1c481296ad36c4b0a00656039b9672dfe26c3f64
adnen rebai <adnen.rebai@gmail.com>
2021-12-22 03:05:10 +0100

    rename list identifier text

--- server/fakeDb.js
+++ server/fakeDb.js
@@ -20,8 +20,8 @@ let users = [
 ];
 
 let lists = [
-  { id: "1640124595272-0cy9trqth", text: "Todo", tasks: [] },
-  { id: "1640124611671-sa10s6543", text: "In progress", tasks: [] },
-  { id: "1640124618637-hn0grc5ai", text: "Done", tasks: [] },
+  { id: "1640124595272-0cy9trqth", title: "Todo", tasks: [] },
+  { id: "1640124611671-sa10s6543", title: "In progress", tasks: [] },
+  { id: "1640124618637-hn0grc5ai", title: "Done", tasks: [] },
 ];
 module.exports = { users, lists };
