2791d47860817d54902b7ec777002d52053cf8e6
adnen rebai <adnen.Rebai@gmail.com>
2022-03-21 23:19:45 +0100

    fix buiding url

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import Theme from "./Theme";
 import "./index.css";
 ReactDOM.render(
   <React.StrictMode>
     <Theme>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Theme>
   </React.StrictMode>,
   document.getElementById("root")
--- src/services/config.ts
+++ src/services/config.ts
@@ -1,1 +1,3 @@
-export const MAIN_URL = process.env.REACT_APP_BACKEND_ENDPOINT;
+export const MAIN_URL =
+  process.env.PORT || process.env.REACT_APP_BACKEND_ENDPOINT;
+console.log(process.env.PORT);
