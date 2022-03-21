196abf65612fc92bc10c1df7131cf842da7402c6
Adnen Rebai <adnen.rebai@gmail.com>
2022-03-21 22:36:32 +0100

    fix logo

--- src/Components/Logo/index.js
+++ src/Components/Logo/index.js
@@ -1,5 +1,7 @@
+import logo from "./trello.png";
+
 const TrelloLogo = () => {
-  return <img src="trello.png" alt="trello logo" width="90px" height="50px" />;
+  return <img src={logo} alt="trello logo" width="90px" height="50px" />;
 };
 
 export default TrelloLogo;
