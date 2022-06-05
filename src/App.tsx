e86adfdc2110d9144987911440a5b4207656a6c4
adnen rebai <adnen.Rebai@gmail.com>
2022-06-05 22:04:55 +0100

    add route resetpassword

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 
 import DashboardPage from "./pages/DashboardPage";
+import ResetPassword from "./pages/ResetPassword";
 const App = () => (
   <Routes>
     <Route path="/Dashboard" element={<DashboardPage />} />
     {/* TODO : add protected route for login page */}
+    <Route path="/resetPassword" element={<ResetPassword />} />
+
     <Route path="/" element={<LoginPage />} />
   </Routes>
 );
