a3f16e156f9be9b08f7c9be33a802c17c7a6ebbb
adnen rebai <adnen.Rebai@gmail.com>
2022-06-05 22:08:24 +0100

    refactor

--- server/index.js
+++ server/index.js
@@ -4,79 +4,101 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const fakeData = require("./fakeDb");
+const { findUser, findUserByEmail } = require("./utility");
+const { checkToken, SECRET_TOKEN } = require("./middleware");
+
 dotenv.config();
 
-// verify header
-// const verifyToken = (req, res, next) => {
-//   const bearerHeader = req.header["authorization"];
-//   if (typeof bearerHeader != "undefined") {
-//     const bearerHeader = bearerHeader.split(" ")[1];
-//     req.token = bearerToken;
-//     next();
-//   }
-// };
-
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-const port = process.env.PORT || 5500;
+const PORT = process.env.PORT;
 
 let lists = [...fakeData.lists];
 let users = [...fakeData.users];
 
-const findUser = (data) => {
-  const user = users.find(({ username }) => username === data.username);
-  return user;
-};
-// FAKE DB USERS
-// REGISTER ROUTE
-app.post("/register", (req, res) => {
-  const user = findUser(req.body);
-  if (user) {
-    res.json({
-      status: 0,
-      message: "User alreay exist !",
-    });
-  } else {
-    const { username, email, password } = req.body;
-    let newUser = { username, email, password };
-    users.push({ id: users.length + 1, ...newUser });
-    res.json({
-      status: 1,
-      message: `${req.body.username} created successfully !`,
-    });
-  }
-});
-// LOGING ROUTE
-app.post("/login", (req, res) => {
-  const user = findUser(req.body);
+class RequestHandler {
+  //LOGIN
+  login(req, res) {
+    const user = findUser(users, req.body);
 
-  if (user) {
-    jwt.sign({ user: user }, "secretkey", (err, accessToken) => {
+    if (user) {
+      let token = jwt.sign({ username: user.username }, SECRET_TOKEN);
       res.json({
         status: 1,
-        accessToken,
+        message: `Authentication successful for username : ${user.username}`,
+        token,
       });
-    });
-  } else {
-    res.json({
-      status: 0,
-      message: "User Not Found !",
-      hints: "try username : trello, password : trello ",
-    });
+    } else {
+      res.json({
+        status: 0,
+        message: "User Not Found !",
+        hints: "try username : trello, password : trello ",
+      });
+    }
   }
-});
+  //REGISETER
+  register(req, res) {
+    const user = findUser(users, req.body);
+    if (user) {
+      res.json({
+        status: 0,
+        message: "User alreay exist !",
+      });
+    } else {
+      const { username, email, password } = req.body;
+      let newUser = { username, email, password };
+      users.push({ id: users.length + 1, ...newUser });
+      res.json({
+        status: 1,
+        message: `${req.body.username} created successfully !`,
+      });
+    }
+  }
 
-// SAVE ROUTE NEEDED WHILE DRAGGING
-app.post("/save", (req, res) => {
-  lists = req.body.lists;
-  return res.json({ success: true });
-});
+  //RESETPASSWORD
+  resetPassword(req, res) {
+    const { username } = findUserByEmail(users, req.body);
+    if (username) {
+      res.json({
+        status: 1,
+        message: "user found !",
+      });
+    } else {
+      res.json({
+        status: 0,
+        message: "email Not Found !",
+      });
+    }
+  }
+  //SAVE
+  // SAVE ROUTE NEEDED WHILE DRAGGING
+  save(req, res) {
+    lists = req.body.lists;
+    return res.json({ success: true });
+  }
+  //LOAD
+  load(req, res) {
+    return res.json({ lists });
+  }
+}
 
-//
-app.get("/load", (req, res) => res.json({ lists }));
+const main = () => {
+  const app = express();
+  const handlers = new RequestHandler();
+  const port = PORT || 5500;
+  app.use(cors());
+  app.use(
+    bodyParser.urlencoded({
+      // Middleware
+      extended: true,
+    })
+  );
+  app.use(bodyParser.json());
+  app.post("/login", handlers.login);
+  app.post("/register", checkToken, handlers.register);
+  app.post("/resetPassword", checkToken, handlers.resetPassword);
+  app.get("/load", checkToken, handlers.load);
+  app.post("/save", checkToken, handlers.save);
 
-const logRunningServer = () => console.log(`Server is running on ${port}`);
+  app.listen(port, () => console.log(`Server is running on ${port}`));
+};
 
-app.listen(port, logRunningServer);
+main();
---
+++ src/AuthenticationContainer/index.tsx
@@ -0,0 +1,64 @@
+import Box from "@mui/material/Box";
+import { Alert, Stack } from "@mui/material";
+
+import {
+  AuthenticationContainer,
+  AuthenticationPage,
+} from "./AuthenticationContainer.styled";
+
+import TrelloLogo from "../Components/Logo";
+type backendResponce = {
+  status: number;
+  message: string;
+  hints?: string;
+};
+type AuthenticationProps = {
+  children: React.ReactNode;
+  backendResponce: backendResponce | null;
+};
+
+const Authentication = ({ backendResponce, children }: AuthenticationProps) => {
+  return (
+    <AuthenticationPage>
+      <AuthenticationContainer>
+        <Box sx={{ width: "400px", background: "white", borderRadius: "10px" }}>
+          <Box
+            sx={{ display: "flex", justifyContent: "center", padding: "1rem" }}
+          >
+            <TrelloLogo />
+          </Box>
+
+          <Box
+            sx={{
+              boxShadow: "0 0 20px 8px #b1b6f39c",
+              width: "350px",
+              margin: "auto",
+              borderRadius: "10px",
+              background: "linear-gradient(45deg, #30b4ff, #c0b6f2)",
+            }}
+          >
+            {children}
+          </Box>
+          <Box
+            sx={{
+              padding: "1rem 0",
+              width: "350px",
+              margin: "auto",
+            }}
+          >
+            <Stack spacing={1}>
+              {backendResponce?.message && (
+                <Alert severity="error"> {backendResponce?.message}</Alert>
+              )}
+              {backendResponce?.hints && (
+                <Alert severity="info">{backendResponce?.hints}</Alert>
+              )}
+            </Stack>
+          </Box>
+        </Box>
+      </AuthenticationContainer>
+    </AuthenticationPage>
+  );
+};
+
+export default Authentication;
