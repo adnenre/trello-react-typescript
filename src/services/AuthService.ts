ce3c7a4ccaaf4bfd902455922fb00fd9ecc5e55d
adnen rebai <adnen.rebai@gmail.com>
2022-12-31 05:00:26 +0100

    refactor : add use-storage-node : store encrypted data

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,47 +1,55 @@
 import axios from 'axios';
-import  MAIN_URL  from './config';
+import MAIN_URL from './config';
 import { IuserLogin, IuserRegister } from '../interfaces';
+import storage from 'use-storage-node';
+import storageKey from './config';
+const key = storageKey || 'storage_key';
+const store = storage(key);
 class AuthService {
     // LOGIN SERVICE
     login = (data: IuserLogin) => {
-        return axios.post(`${MAIN_URL}/login`, { ...data }).then((response) => {
-            if (response.status) {
-                localStorage.setItem('token', response.data.token);
-            }
-            console.log(response)
-            return response.data;
-        }).catch(error => {
-            // Handle error
-            return(error);
-          })
+        return axios
+            .post(`${MAIN_URL}/login`, { ...data })
+            .then((response) => {
+                if (response.status) {
+                    store.setItem('token', response.data.token);
+                }
+                console.log(response);
+                return response.data;
+            })
+            .catch((error) => {
+                // Handle error
+                return error;
+            });
     };
     register = (data: IuserRegister) => {
         return axios
             .post(`${MAIN_URL}/register`, { ...data })
             .then((response) => {
                 if (response.status) {
-                    localStorage.setItem('token', response.data.token);
+                    store.setItem('token', response.data.token);
                 }
-               
+
                 return response.data;
-            }).catch(error => {
+            })
+            .catch((error) => {
                 // Handle error
-                return(error);
-              })
+                return error;
+            });
     };
     resetPassword = (email: string) => {
         return axios
             .post(`${MAIN_URL}/resetPassword`, { email })
             .then((response) => {
                 if (response.status) {
-                    localStorage.setItem('token', response.data.token);
+                    store.setItem('token', response.data.token);
                 }
                 return response.data;
             });
     };
     // LOGOUT SERVICE
     logout = () => {
-        localStorage.removeItem('token');
+        store.removeItem('token');
     };
 }
 
--- src/services/config.ts
+++ src/services/config.ts
@@ -1,2 +1,5 @@
 const MAIN_URL = process.env.REACT_APP_API === "DEV_URL" ? process.env.REACT_APP_API_DEV : process.env.REACT_APP_API_PROD;
+
+export const storageKey : string | undefined = process.env.REACT_APP_STORAGE_KEY  ;
+
 export default MAIN_URL;
